b2a352a53bc2de21fe01be56e023360d1b407360
Victoria Pham <victoriapham52@gmail.com>
2023-03-14 16:40:09 -0700

    Redid Exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -9,37 +9,64 @@
             //int a = 17;
             //int b = 4;
 
-            Console.WriteLine($"Addition: a + b = {a + b}");
-            Console.WriteLine($"Subtraction: a - b = {a - b}");
-            Console.WriteLine($"Multiplication: a * b = {a * b}");
-            Console.WriteLine($"Dvision: a / b = {a / b}");
-            Console.WriteLine($"Modulus: a % b = {a % b} \n ");
+            //Console.WriteLine($"Addition: a + b = {a + b}");
+            //Console.WriteLine($"Subtraction: a - b = {a - b}");
+            //Console.WriteLine($"Multiplication: a * b = {a * b}");
+            //Console.WriteLine($"Dvision: a / b = {a / b}");
+            //Console.WriteLine($"Modulus: a % b = {a % b} \n ");
 
-            int quotient = a / b;
-            int remainder = a % b;
+            //int quotient = a / b;
+            //int remainder = a % b;
 
-            Console.WriteLine($"{a}/{b} is {quotient} remainder of {remainder}");
+            //Console.WriteLine($"{a}/{b} is {quotient} remainder of {remainder}");
 
+            //Exercise 2 - Call - version 1
 
-            //Exercise 2
+            //Console.WriteLine("What is the radius of your circle?");
+            //var userRadius = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("What is the radius of your circle?");
-            var radius = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"The area of a circle with a radius of {userRadius} is {AreaOfCircle(userRadius)}.");
 
-
-            double AreaOfCircle = Math.PI * Math.Pow(radius, 2);
-            Console.WriteLine($"The area of a circle with radius of {radius} is {AreaOfCircle}");
-
+            //Exercise 2 - call - version 2
+            AreaOfCircle();
+            
             //Thought Exercise
-            var i = 3;
-            var j = 4;
-            var k = ++i * j++;
+            //var i = 3;
+            //var j = 4;
+            //var k = ++i * j++;
 
-            Console.WriteLine(k); // k = 16
-
-
-
+            //Console.WriteLine(k); // k = 16
 
         }
+        //Exercise 2 - version 1
+        //public static double AreaOfCircle(double radius)
+
+        //{
+
+        //    var area = Math.PI * Math.Pow(radius, 2);
+        //    return area;
+
+        //}
+
+        //Exercise 2 - version 2
+        public static double AreaOfCircle()
+        {
+            
+            Console.WriteLine($"What is your radius?");
+            var radius = double.Parse(Console.ReadLine());
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of a cirlce with a radius of {radius} is {area}.");
+
+            return area;
+
+        }
+
+
+
     }
 }
+
+
+
